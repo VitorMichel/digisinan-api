0a5f171db05b25e75fc0474d075f8bfdb06d361a
Vitor Michel <vitor.michel@valtech.com>
2022-03-14 19:20:08 -0300

    rollback

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GETPUT,POST,DELETE');
     // res.header("Access-Control-Allow-Headers: Content-Type");
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    // res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
     app.use(cors());
     next();
 });
