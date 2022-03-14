7e780491eb1ad266e655c1f7aaf33ccc032ced69
Vitor Michel <vitor.michel@valtech.com>
2022-03-14 18:58:01 -0300

    create option for cors

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express();
 
 let port = process.env.PORT || 3333;
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use(express.json());
 app.use(routes);
 app.listen(port, () => {
