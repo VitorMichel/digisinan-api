b7db9312db71a94f21f7f2312ceeb2dbb56d4469
Vitor Michel <vitor.michel@valtech.com>
2022-03-14 19:24:49 -0300

    ajustes

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ let port = process.env.PORT || 3333;
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GETPUT,POST,DELETE');
+    res.header("Access-Control-Allow-Methods", 'GET, PUT ,POST ,DELETE');
     // res.header("Access-Control-Allow-Headers: Content-Type");
-    // res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
     app.use(cors());
     next();
 });
