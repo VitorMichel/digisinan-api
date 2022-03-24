50fb46664cdb2ded3943c3522a8e9b6cb43ddaa1
Vitor Michel <vitor.michel@valtech.com>
2022-03-24 15:36:04 -0300

    rollback

--- src/createLinkPdf.js
+++ src/createLinkPdf.js
@@ -6,7 +6,7 @@ async function criarLink() {
     let stream = fs.createWriteStream('./sample.pdf');
     let pdf = new PDFDocument({ autoFirstPage: false });
     pdf.pipe(stream);
-    let imagemAqui = await axios.get('https://img.elo7.com.br/product/original/3249FFD/quadro-decorativo-natureza-paisagem-mar-sol-coqueiro-ponte-quadro-belezas-naturais.jpg', { responseType: 'arraybuffer' });
+    let imagemAqui = '././template/pagina1.jpeg';
     pdf.image(imagemAqui, 150, 200, { width: 300 });
     pdf.end();
 }
