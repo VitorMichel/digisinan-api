1d4da4f4155c9c6d935883aa1369a8126985a435
Vitor Michel <vitor.michel@valtech.com>
2022-03-28 19:40:07 -0300

    ultimo verdade

--- src/createLinkPdf.js
+++ src/createLinkPdf.js
@@ -19,6 +19,10 @@ async function criarLink() {
     const logo = await fetchImage("https://wigwsxuobmtlhlcdigsa.supabase.co/storage/v1/object/sign/files/pdf1.jpeg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJmaWxlcy9wZGYxLmpwZWciLCJpYXQiOjE2NDg1MDU1NzEsImV4cCI6MTk2Mzg2NTU3MX0.lKZtX-wOaFzQ2nauVMcVWP3QwwwrzODx7mgmQuNanKk");
     doc.image(logo, 1, 1, { width: 610, height: 800 });
 
+    doc.addPage({ margin: 5 });
+    const pagina2 = await fetchImage("https://wigwsxuobmtlhlcdigsa.supabase.co/storage/v1/object/sign/files/pdf2.jpeg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJmaWxlcy9wZGYyLmpwZWciLCJpYXQiOjE2NDg1MDY5MTQsImV4cCI6MTk2Mzg2NjkxNH0.yuYv9_dlMQJSm7m1t4jx9IuWZvByIybO5o57w8uAX4Y");
+    doc.image(pagina2, 1, 1, { width: 610, height: 800 });
+
     doc.end();
 }
 
