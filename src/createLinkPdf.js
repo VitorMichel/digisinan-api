67288b8bd21d68298282b143a426c862f84852cb
Vitor Michel <vitor.michel@valtech.com>
2022-04-03 22:01:21 -0300

    ajust

--- src/createLinkPdf.js
+++ src/createLinkPdf.js
@@ -22,6 +22,7 @@ async function criarLink() {
     doc.addPage({ margin: 5 });
     const pagina2 = await fetchImage("https://wigwsxuobmtlhlcdigsa.supabase.co/storage/v1/object/sign/files/pdf2.jpeg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJmaWxlcy9wZGYyLmpwZWciLCJpYXQiOjE2NDg1MDY5MTQsImV4cCI6MTk2Mzg2NjkxNH0.yuYv9_dlMQJSm7m1t4jx9IuWZvByIybO5o57w8uAX4Y");
     doc.image(pagina2, 1, 1, { width: 610, height: 800 });
+    doc.text('nomeProfissional', 67, 767, { width: 320 }); // campo 82 profissional
 
     doc.end();
 }
