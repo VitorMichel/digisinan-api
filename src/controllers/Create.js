0cc154deefe59e6278c94618d0f1596c7937cc3a
Vitor Michel <vitor.michel@valtech.com>
2022-07-11 19:59:58 -0300

    enbdpoint

--- src/controllers/Create.js
+++ src/controllers/Create.js
@@ -4,14 +4,7 @@ const connection = getConnection();
 module.exports = {
     async getUserData(request, response)
     {
-        let query = 'SELECT * FROM ESTADO';
-        console.log(query);
+        console.log(response);
 
-        connection.query(query, function (error, results, fields) {
-            if (error) 
-                return response.json({ status: 404, message: error.message });
-                
-            return response.json(results);
-        });
     }
 };
