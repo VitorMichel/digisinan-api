8eb32a80723533deaf419af67ad7718585b2566c
Vitor Michel <vitor.michel@valtech.com>
2022-07-18 19:39:13 -0300

    endpoint create user

--- src/controllers/Create.js
+++ src/controllers/Create.js
@@ -4,7 +4,16 @@ const connection = getConnection();
 module.exports = {
     async getUserData(request, response)
     {
-        console.log(response);
+        const { data } = request.body;
 
+        let query = 'INSERT INTO USUARIO (NM_USUARIO,      NM_USUARIO_SOCIAL,       DS_SENHA,        NR_CPF,         DS_EMAIL,           CD_CONSELHO,             NR_CONSELHO,           CD_UF,          IM_ASSINATURA)';
+        query +=    'VALUES              ('${data.nome}', '${data.nomeSocial}', '${data.senha}', '${data.cpf}', '${data.email}', '${data.codigoConselho}', '${data.numeroConselho}', '${data.uf}', '${data.imagem_assinatura}');'
+
+        connection.query(query, function (error, results, fields) {
+            if (error) 
+                return response.json({ status: 404, message: error.message });
+                
+            return response.json(results);
+        });
     }
 };
