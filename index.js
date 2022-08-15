3c8b0868527dec3949eb7c3f415c0799713fd3be
Vitor Michel <vitor.michel@valtech.com>
2022-08-15 20:11:48 -0300

    tirar localhost

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     // res.header("Access-Control-Allow-Headers: Content-Type");
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-app.use(cors('localhost'));
+// app.use(cors('localhost'));
     next();
 });
 app.use(express.json());
