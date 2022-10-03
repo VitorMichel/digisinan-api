30d14d389bd4930895faea7c7c5db909c04240ce
Vitor Michel <vitor.michel@valtech.com>
2022-10-03 20:06:35 -0300

    ajuste

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express();
 let port = process.env.PORT || 3333;
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
     res.header('Access-Control-Allow-Credentials', true);
