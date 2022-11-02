f130c3fcdf56b5907768b14f8f891f015b43d5f4
Vitor Michel <vitor.michel@valtech.com>
2022-11-02 19:13:36 -0300

    teste

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    app.use(cors());
+    // app.use(cors());
     next();
 });
 app.use(express.json());
