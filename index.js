41d448721cd63c069449b9201c8d785ec4f8080e
Vitor Michel <vitor.michel@valtech.com>
2022-11-02 20:11:50 -0300

    teste

--- index.js
+++ index.js
@@ -4,24 +4,14 @@ var cors = require('cors');
 const app = express();
 let port = process.env.PORT || 3333;
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-//     // res.header("Access-Control-Allow-Headers: Content-Type");
-//     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-//     app.use(cors());
-//     next();
-// });
-
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    // res.header("Access-Control-Allow-Headers: Content-Type");
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    app.use(cors());
+    next();
+});
 app.use(express.json());
 app.use(routes);
 app.listen(port, () => {
